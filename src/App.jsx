9d975875ced574da6f9932f7f2f07dfee90178f1
TheJB4 <juanmer382@gmail.com>
2023-05-20 09:13:46 -0300

    otra mas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       useEffect(() => {
-        fetch('http://localhost:3500/graphql/categories').then(res => res.json()).then(data=> console.log(data))
+        // fetch('http://localhost:3500/graphql/categories').then(res => res.json()).then(data=> console.log(data))
       }, []);
     </>
   )
